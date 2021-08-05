5ed38944096546914a8f2332a034e04b1eee125a
wujiajie <848420065@qq.com>
2021-08-05 17:27:41 +0800

    'init'

---
+++ src/ComposerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Wusit\ComposerTest;
+
+class ComposerTest
+{
+    public static function debug($val)
+    {
+        var_dump($val);
+        die;
+    }
+}
---
+++ test/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+$val = "test";
+
+\Wusit\ComposerTest\ComposerTest::debug($val);
+
+echo 'end';
